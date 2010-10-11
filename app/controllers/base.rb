80c660a9f0dbb0131eeab15e9c20be2bcd4cfc62
Ian Serlin <ian@ianserlin.com>
2010-10-10 23:22:48 -0700

    whats up dog

---
+++ app/controllers/base.rb
@@ -0,0 +1,21 @@
+KolaboratoryCom.controllers do
+  get :index do
+    render 'base/index'
+  end
+  
+  # get :sample, :map => "/sample/url", :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   "Maps to url '/foo/#{params[:id]}'"
+  # end
+
+  # get "/example" do
+  #   "Hello world!"
+  # end
+
+  
+end
